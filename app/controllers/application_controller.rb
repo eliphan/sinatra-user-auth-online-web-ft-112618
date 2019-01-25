78e2ce575e993965d39ac92deb4fea5f3745a5de
Elise Phan <aliceconnic@gmail.com>
2019-01-25 04:13:50 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,6 +30,8 @@ class ApplicationController < Sinatra::Base
     @user = User.find_by(email: params["email"], password: params["password"])
     session[:id] = @user.id
     redirect '/users/home'
+    
+    
   end
 
   get '/sessions/logout' do 
