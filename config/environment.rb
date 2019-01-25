1d71e8c2ac424f4bdd01f15c3c4b07c7db89212d
Elise Phan <aliceconnic@gmail.com>
2019-01-25 03:42:20 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,7 @@ require 'bundler/setup'
 Bundler.require
 
 ENV['SINATRA_ENV'] ||= "development"
+
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/user_auth#{ENV['SINATRA_ENV']}.sqlite"
@@ -9,4 +10,3 @@ ActiveRecord::Base.establish_connection(
 
 Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
 Dir[File.join(File.dirname(__FILE__), "../app/controllers", "*.rb")].each {|f| require f}
-
